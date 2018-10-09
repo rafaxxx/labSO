5929d790701b6cff342ac6de5b37bd789f7ef65e
Victor Hugo <victorhundo@gmail.com>
2018-10-09 08:58:45 -0300

    Corrigido conflitos

--- lab_mem/page_replacement/python/phymem.py
+++ lab_mem/page_replacement/python/phymem.py
@@ -14,42 +14,24 @@ class PhysicalMemory:
   def __init__(self, algorithm):
     assert algorithm in {"fifo", "nru", "aging", "second-chance"}
     self.algorithm = algorithm
-<<<<<<< HEAD
-=======
     if(self.algorithm == "fifo"):
         self.policy = Fifo()
->>>>>>> da5a07bd6c72d0778b602bef05dfa9fc9cd81ee4
 
   def put(self, frameId):
     """Allocates this frameId for some page"""
     # Notice that in the physical memory we don't care about the pageId, we only
     # care about the fact we were requested to allocate a certain frameId
-<<<<<<< HEAD
-    pass
-=======
     self.policy.put(frameId)
->>>>>>> da5a07bd6c72d0778b602bef05dfa9fc9cd81ee4
 
   def evict(self):
     """Deallocates a frame from the physical memory and returns its frameId"""
     # You may assume the physical memory is FULL so we need space!
     # Your code must decide which frame to return, according to the algorithm
-<<<<<<< HEAD
-    pass
-=======
     return self.policy.evict()
->>>>>>> da5a07bd6c72d0778b602bef05dfa9fc9cd81ee4
 
   def clock(self):
     """The amount of time we set for the clock has passed, so this is called"""
     # Clear the reference bits (and/or whatever else you think you must do...)
-<<<<<<< HEAD
-    pass
-
-  def access(self, frameId, isWrite):
-    """A frameId was accessed for read/write (if write, isWrite=True)"""
-    pass
-=======
     self.policy.clock()
 
   def access(self, frameId, isWrite):
@@ -71,4 +53,3 @@ class Fifo:
 
     def access(self, frameId, isWrite):
         pass
->>>>>>> da5a07bd6c72d0778b602bef05dfa9fc9cd81ee4
