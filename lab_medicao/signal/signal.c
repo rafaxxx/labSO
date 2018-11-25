93da977bdc912caed86906bd879908b8d8fa8898
victorhundo <victorhundo@gmail.com>
2018-11-25 11:04:03 -0300

    Refatorado e adicionado lógica cliente-servidor

--- lab_medicao/signal/signal.c
+++ lab_medicao/signal/signal.c
@@ -4,52 +4,63 @@
  */
 
 #include <unistd.h>
+#include <stdio.h>
 #include <signal.h>
 #include <sys/signalfd.h>
-#include <string.h>
-
-int main (int argc, char *argv[])
-{
-        int sfd;
-        sigset_t mask;
-
-        /* We will handle SIGTERM and SIGINT. */
-        sigemptyset (&mask);
-        sigaddset (&mask, SIGINT);
-        sigaddset (&mask, SIGTERM);
-        sigaddset (&mask, SIGQUIT);
+#include <sys/wait.h>
 
 
-         printf("My process ID : %d\n", getpid());
-        /* Block the signals thet we handle using signalfd(), so they don't
-         * cause signal handlers or default signal actions to execute. */
-        sigprocmask(SIG_BLOCK, &mask, NULL);
 
-        /* Create a file descriptor from which we will read the signals. */
-        sfd = signalfd (-1, &mask, 0);
+int server(int sfd) {
+  while (1) {
+     /** The buffor for read(), this structure contains information
+     * about the signal we've read. */
+    struct signalfd_siginfo si;
 
-        while (1) {
+    ssize_t res;
+    res = read (sfd, &si, sizeof(si));
 
-                /** The buffor for read(), this structure contains information
-                 * about the signal we've read. */
-                struct signalfd_siginfo si;
+    if (si.ssi_signo == SIGTERM){
+      printf ("Got SIGTERM\n");
+    }
+    else if (si.ssi_signo == SIGINT) {
+      printf ("Bye!\n");
+      break;
+    }
+  }
+  /* Close the file descriptor if we no longer need it. */
+  close (sfd);
+  return 0;
+}
 
-                ssize_t res;
+int client(int pid, int sfd){
+  while(1){
+    int check = waitpid(pid,0,WNOHANG); /* WNOHANG def'd in wait.h */
+    if (check < 0) break;
+    kill(pid, SIGTERM);
+    sleep(1);
+  }
+}
 
-                res = read (sfd, &si, sizeof(si));
+int main (int argc, char *argv[]) {
+  int sfd;
+  sigset_t mask;
+  printf("My process ID : %d\n", getpid());
 
-                if (si.ssi_signo == SIGTERM){
-                        printf ("Got SIGTERM\n");
-                }
-                else if (si.ssi_signo == SIGINT) {
-                        printf ("Bye!\n");
-                        break;
-                }
-                else if (si.ssi_signo == SIGQUIT) {
-                        printf ("Nem Morreu!\n");
-                }
-        }
-        /* Close the file descriptor if we no longer need it. */
-        close (sfd);
-        return 0;
+  /* We will handle SIGTERM and SIGINT. */
+  sigemptyset (&mask);
+  sigaddset (&mask, SIGINT);
+  sigaddset (&mask, SIGTERM);
+
+  /* Block the signals thet we handle using signalfd(), so they don't
+   * cause signal handlers or default signal actions to execute. */
+  sigprocmask(SIG_BLOCK, &mask, NULL);
+
+  /* Create a file descriptor from which we will read the signals. */
+  sfd = signalfd (-1, &mask, 0);
+
+	int PID=fork();
+	if(PID == 0) server(sfd);
+	else client(PID,sfd);
+  return 0;
 }
