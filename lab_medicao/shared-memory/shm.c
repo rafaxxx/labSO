ae4290e7b360bf48c06d66196e080d21771f17ff
Victor Hugo <victorhundo@gmail.com>
2018-11-28 15:36:25 -0300

    servidor dormi cliente envia

--- lab_medicao/shared-memory/shm.c
+++ lab_medicao/shared-memory/shm.c
@@ -12,21 +12,47 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/wait.h>
+#include <time.h>
 
-int read_shm(int * shm_ptr){
-  return (int) shm_ptr[0];
+int const FLAG_SERVER = 1;
+int const FLAG_CLIENT = 0;
+int const INDEX_FLAG = 0;
+int const INDEX_DATA_SERVER = 1;
+int const INDEX_DATA_CLIENT = 2;
+
+int read_shm(int * shm_ptr, int index){
+  return (int) shm_ptr[index];
 }
 
-void write_shm(int * shm_ptr, int data){
-  shm_ptr[0] = data;
+void write_shm(int * shm_ptr, int index, int data){
+  shm_ptr[index] = data;
 }
 
 int server(int * shm_ptr) {
+  srand(time(NULL));   // Initialization, should only be called once.
+
+  int sleep_time = 2;
+  int previous_data = 0;
+  int sleep_count = 0;
   while (1) {
-    int data = read_shm(shm_ptr);
-    if( data == 5 ) break;
-    printf("Data read in shared memory (index 0): %d\n",data);
-    sleep(2);
+    int valor = read_shm(shm_ptr, INDEX_DATA_CLIENT);
+
+    if (valor != previous_data){
+      printf("[Server] - Recebi o valor %d do cliente\n",valor);
+      previous_data = valor;
+
+      if (sleep_count >= 5){
+        int sleep_time = rand() % 10;
+        write_shm(shm_ptr,INDEX_FLAG,FLAG_SERVER);
+        write_shm(shm_ptr,INDEX_DATA_SERVER, sleep_time);
+        printf("[Server] - Vou dormir por %d segundos\n",sleep_time + 1); // +1 por causa do ultimo sleep
+        sleep(sleep_time);
+        sleep_count = 0;
+        write_shm(shm_ptr,INDEX_FLAG,FLAG_CLIENT);
+      }
+      sleep_count++;
+    }
+    sleep(1);
   }
   return 0;
 }
@@ -37,12 +63,18 @@ int client(int pid, int * shm_ptr){
     int check = waitpid(pid,0,WNOHANG); /* WNOHANG def'd in wait.h */
     if (check < 0) break;
 
-    write_shm(shm_ptr, count++);
-    sleep(2);
+    if(read_shm(shm_ptr, INDEX_FLAG) == FLAG_CLIENT){
+      printf("[Cliente] - Enviando %d... \n", count);
+      write_shm(shm_ptr, INDEX_DATA_CLIENT, count++);
+    }else{
+      int sleep_time = read_shm(shm_ptr, INDEX_DATA_SERVER);
+      sleep(sleep_time);
+    }
+
+    sleep(1);
   }
   return 0;
 }
-
 
 int main (int argc, char *argv[]) {
   printf("My process ID : %d\n", getpid());
@@ -54,6 +86,10 @@ int main (int argc, char *argv[]) {
   key_t key = ftok((char *) "shm.c", 66);
   int shmid = shmget(key, 4097, 0777 | IPC_CREAT ); // Cria a memoria compartilhada
   int *shm_ptr = (int *) shmat(shmid, NULL, 0); // Anexa a memoria compartilhada e retorna enderço de memoria
+
+  write_shm(shm_ptr,INDEX_FLAG,0);
+  write_shm(shm_ptr,INDEX_DATA_CLIENT,0);
+  write_shm(shm_ptr,INDEX_DATA_SERVER,0);
 
   int PID=fork();
   if(PID == 0) server(shm_ptr);
